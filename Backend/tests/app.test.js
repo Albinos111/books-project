effb2c94d52e3416a1268fa32127f2fee3d3d564
Evgeniy <lammaincapoo38@gmail.com>
2026-04-07 01:08:28 +0300

    add postgres service to CI

--- Backend/tests/app.test.js
+++ Backend/tests/app.test.js
@@ -1,5 +1,3 @@
-
-
 const request = require("supertest");
 const app = require("../app");
 
@@ -12,3 +10,18 @@ describe("GET /api/time", () => {
     expect(res.body).toHaveProperty("minute");
   });
 });
+
+describe("POST /api/auth/register", () => {
+  it("should register a new user", async () => {
+    const res = await request(app)
+      .post("/api/auth/register")
+      .send({
+        username: `user_${Date.now()}`,
+        password: "123456"
+      });
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toHaveProperty("message");
+  });
+});
+
